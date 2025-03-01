b6074eb0d4b8d566f2c5a3ca72f9ce9610df7a95
ahmed osama <ahmedosama21049@gmail.com>
2025-03-01 14:03:22 +0200

    Updated authentication middleware to support direct token usage

--- middlewares/AuthMiddleware.js
+++ middlewares/AuthMiddleware.js
@@ -4,19 +4,21 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const userAuth = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
+  let token = req.headers.authorization;
 
   if (!token) {
-    const error = "Not authorized, token missing";
-    res.status(401);
-    return next(error);
+    return res.status(401).json({ message: "Not authorized, token missing" });
   }
+
+  if (token.startsWith("Bearer ")) {
+    token = token.split(" ")[1];
+  }
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (error) {
-    res.status(401);
-    return next(new Error("Invalid Token"));
+    return res.status(401).json({ message: "Invalid Token" });
   }
 };
