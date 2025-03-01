760c9e141e208e406a56fda51625ad0e8c5024e1
Hendawyy <seifhendawy1@gmail.com>
2025-03-01 10:22:39 +0200

    Edit The App to Use The Error Handler Middleware

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const app = require('express')();
+import express from "express";
+import { errHandler } from "./middlewares/ErrHandlerMiddleware.js";
+
+const app = express();
+app.use(express.json);
+
+app.use(errHandler);
--- middlewares/AuthMiddleware.js
+++ middlewares/AuthMiddleware.js
@@ -5,15 +5,18 @@ dotenv.config();
 
 export const userAuth = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
-  
-  if (!token) 
-    return res.status(401).json({ message: "Not authorized, token missing" });
-  
+
+  if (!token) {
+    const error = "Not authorized, token missing";
+    res.status(401);
+    return next(error);
+  }
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (error) {
-    res.status(401).json({ message: "Invalid token" });
+    res.status(401);
+    return next(new Error("Invalid Token"));
   }
 };
