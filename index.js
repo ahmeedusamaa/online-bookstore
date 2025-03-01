cf255d3abb225192cf2756b68ee7a46a0d33aa88
philopater mansour <philopater.mansour@gmail.com>
2025-03-01 14:48:26 +0200

    add Mongoose schemas for users, books, reviews, authors, and orders

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from "express";
 import { errHandler } from "./middlewares/ErrHandlerMiddleware.js";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 app.use(express.json);
+
+mongoose.connect(`mongodb+srv://${process.env.ATLAS_USER_NAME}:${process.env.ATLAS_PASSWORD}@online-bookstore.ql6vl.mongodb.net/online-bookstore?retryWrites=true&w=majority&appName=online-bookstore`);
+mongoose.connection.on('error', (err) => {
+    console.error('MongoDB connection error: ', err);
+    process.exit(1);
+});
 
 app.use(errHandler);
