9d48d6709804d4d3e398dfdc4fffa0a00cc72c22
philopater mansour <philopater.mansour@gmail.com>
2025-03-01 23:50:38 +0200

    feat: Add cart and order controllers

---
+++ controllers/orders.js
@@ -0,0 +1,31 @@
+import Order from "../models/OrderModel.js";
+import Book from "../models/BookModel.js";
+
+const create = async (orderData, userId) => {
+    const { books, totalPrice } = orderData;
+
+    if (!books || books.length === 0 || totalPrice < 0) {
+        throw new Error("Invalid order data");
+    }
+
+    const validBooks = await Book.find({ _id: { $in: books } });
+    if (validBooks.length !== books.length) {
+        throw new Error("Book or more not found");
+    }
+
+    return Order.create({ User_ID: userId, Books: books, TotalPrice: totalPrice });
+};
+
+const getMany = (skip, limit) => {
+    return Order.find().populate("User_ID", "Name Email").populate("Books").skip(skip).limit(limit);
+};
+
+const getById = (orderId) => {
+    return Order.findById(orderId).populate("User_ID", "Name Email").populate("Books");
+};
+
+const remove = (orderId) => {
+    return Order.findByIdAndDelete(orderId);
+};
+
+export default { create, getMany, getById, getByUserId, remove };
--- routes/orders.js
+++ routes/orders.js
@@ -1,13 +1,43 @@
 import express from 'express';
+import userAuth from '../middlewares/AuthMiddleware.js';
+
 
 const ordersRouter = express.Router();
 
-ordersRouter.post('/', (req, res) => {
-
+ordersRouter.post("/", userAuth, async (req, res, next) => {
+    await ordersController.create(req.body, req.user.id)
+        .then(data => res.status(201).send({ status: "success", data }))
+        .catch(error => next(error));
 });
 
-ordersRouter.get('/:id', (req, res) => {
+ordersRouter.get("/", userAuth, async (req, res, next) => {
+    if (req.user.Role !== "admin") {
+        return res.status(403).send({ status: "error", message: "Access denied" });
+    }
+    const limit = req.query.limit || 5;
+    const skip = ((req.query.page || 1) - 1) * limit;
+    await ordersController.getMany(skip, limit)
+        .then(data => res.status(200).send({ status: "success", data }))
+        .catch(error => next(error));
+});
 
+ordersRouter.get("/:id", userAuth, async (req, res, next) => {
+    if (req.user.Role !== "admin") {
+        return res.status(403).send({ status: "error", message: "Access denied" });
+    }
+    await ordersController.getById(req.params.id)
+        .then(data => res.status(200).send({ status: "success", data }))
+        .catch(error => next(error));
+});
+
+ordersRouter.delete("/:id", userAuth, async (req, res, next) => {
+    if (req.user.Role !== "admin") {
+        return res.status(403).send({ status: "error", message: "Access denied" });
+    }
+
+    await ordersController.remove(req.params.id)
+        .then(() => res.status(200).send({ status: "success", message: "Order deleted" }))
+        .catch(error => next(error));
 });
 
 export default ordersRouter;
