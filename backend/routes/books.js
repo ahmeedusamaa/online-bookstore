92fb58789c7a5111a17fad0625c74e145a73776a
philopater mansour <philopater.mansour@gmail.com>
2025-03-08 18:35:51 +0200

    Fix: Get Books error

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -35,16 +35,14 @@ booksRouter.post(
   }
 );
 
-booksRouter.get("/", async (req, res, next) => {
+booksRouter.get('/', userAuth, async (req, res, next) => {
   const limit = req.query.limit || 4;
   const skip = ((req.query.page || 1) - 1) * limit;
-  await booksController
-    .getMany(req.body, skip, limit)
-    .then((data) => {
-      logger.info(`User with id [${req.user.id}] read books data`);
-      res.status(200).send({ status: "success", data: data });
-    })
-    .catch((error) => next(error));
+  await booksController.getMany(req.body, skip, limit)
+    .then(data => {
+      logger.info(`User with id[${req.user.id}] read books data`);
+      res.status(200).send({ status: "success", data: data })
+    }).catch(error => next(error));
 });
 
 booksRouter.get("/:id", userAuth, async (req, res, next) => {
