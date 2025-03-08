d7614a987c0d585af69225ec550bf198f7be6359
philopater mansour <philopater.mansour@gmail.com>
2025-03-08 22:42:11 +0200

    deploy frontend

--- backend/utils/tokenUtil.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export const generateToken = (payload) => {
-  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
-};
